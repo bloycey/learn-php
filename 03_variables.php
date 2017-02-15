1e4061c9b512e5d69b29baebc49b9bce04eb2d75
bloycey <bloyce1990@gmail.com>
2017-02-15 21:34:37 +1000

    Initial Commit

---
+++ 03_variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Introduction to PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    
+    <div class="container">
+          <h1>Up & Running with PHP</h1>
+          
+          <div class="code-content">
+              <?php 
+                $name = "Andrew";
+                $favourite_number = 8;
+                $happy = true;
+              
+                echo $name;
+                echo "<hr>";
+                echo $favourite_number;
+                echo "<hr>";
+                echo $happy;
+              ?>
+              
+          </div>
+    </div>
+    
+  </body>
+</html>
